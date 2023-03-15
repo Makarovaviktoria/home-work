28182234ce29bd26f2a996db31c07ddb1d3cc41a
Makarovaviktoria <123586940+Makarovaviktoria@users.noreply.github.com>
2023-03-15 16:32:49 +0100

    03.03.23
     1 уровень сложности: Задание 1:
    
    
    Создать переменные типа boolean, int, String, double.
    
    Каждой переменной задать значение в соответствии с этим типом.
    
    Вывести значение каждой переменной на экран. 
    
    
    Задание 2:
    
    
    
    Написать 4 метода в любом классе каждый из которых соответствует определённо математической операции(+, -, /, *) - сложение, вычитание, деление, умножение. 
    
    В каждом методе две целочисленные переменные должны быть созданы с определённым значением, с ними должна быть проведена математическая операция, соответствующая  названию метода и выведен результат.
    
    Эти методы должны быть вызваны из метода "main".
    
    
    2 уровень сложности: 
    
    Dobry den . Ia ne dodela zadanie (2)polnostiu chto to u menia stala zakrivatsia programma .

---
+++ Tel_Ran 1 01.03.23/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int number1 = 2860;
+        int number2 = 300;
+        int resultOfAddition = number1 + number2;
+        System.out.println(resultOfAddition);
+    }
+}
---
+++ Tel_ran 01.03.23/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+                String etc = "red umbrella";
+                String etc2 ="1312141516";
+                int number = 10;
+                int number1 = 11;
+                boolean b1 = true;
+                boolean b2 = false;
+                double Number1 = 8;
+                double Number3 = 19;
+                System.out.println(Number1 /Number3);
+                System.out.println(number);
+                System.out.println(number1);
+                System.out.println(b1);
+                System.out.println(b2);
+                System.out.println(etc);
+                System.out.println(etc2);
+        System.out.println("Hello world!");
+    }
+}
